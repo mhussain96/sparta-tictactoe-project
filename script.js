44a1a620087174fae7bf68a8e3b0ca329c847b95
Maiwand <mhussain2@spartaglobal.com>
2019-01-16 22:02:46 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  let playerTurn = true;
+  const player1 = 'X';
+  const player2 = 'O';
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
+  const squares = document.querySelectorAll('td');
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", function(e) {
+      document
+      console.log(e.target);
+    })
+  }
+  
+
+  // PSEUDOCODE
+  // 1. Get all td elements
+  // const squares = document.getElementsByTagName("td");
+  // // console.log(squares);
+
+  // // 2. add clicks to each element
+  // for (let i = 0; i < squares.length; i++) {
+
+  //   squares[i].addEventListener("click", function (e) {
+  //     // 3. when you click a square, fill with an X
+  //     // 4. when you click the next square, fill with O      
+  //     console.log(e.target)
+  //   })
+  // }
+
+})
