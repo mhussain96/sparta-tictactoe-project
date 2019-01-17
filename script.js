93f961920638b7000719b5d57a073e8c664af49f
Maiwand <mhussain2@spartaglobal.com>
2019-01-17 00:25:35 +0000

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   let playerTurn = true;
-  const player1 = 'X';
-  const player2 = 'O';
-  const winCombos = [
+  const winCombos = [ 
     [0, 1, 2],   // 0
     [3, 4, 5],   // 1
     [6, 7, 8],   // 2
@@ -14,15 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
     [6, 4, 2]    // 7
   ]  // array of arrays
 
-  const squares = document.querySelectorAll('td');
-  
-  for (let i = 0; i < squares.length; i++) {
+  const squares = document.querySelectorAll('td'); // storing the array of td's in squares 
+  // td represents the individual grid squares
+  for (let i = 0; i < squares.length; i++) {  // for each square the for loop adds an event listener 
     squares[i].addEventListener("click", function(e) {
-      // document.getElementsByTagName()
-      // console.log(e.target);
 
-      if (playerTurn) {
-        e.target.className = 'X';
+      if (playerTurn) { // e.target targets the square we clicked on 
+        e.target.className = 'X'; 
         e.target.innerText = 'X';
         playerTurn = false; // when player1 fills box this statement allows player2 to fill box
       } else if (!playerTurn) {
@@ -31,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         playerTurn = true;
       }
       for (let i = 0; i < winCombos.length; i++) { // for loop for winning combos
-        if (squares[winCombos[i][0]].innerText == 'X' &&
+        if (squares[winCombos[i][0]].innerText == 'X' && // squares with the winning combination of i that is between 0 and 7 in our winCombos array
         squares[winCombos[i][1]].innerText == 'X' &&
         squares[winCombos[i][2]].innerText == 'X') {
         alert('X Wins!'); 
@@ -41,24 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
         alert('O Wins!')  
         }
       }
-
     })
   }
-  
-
-  // PSEUDOCODE
-  // 1. Get all td elements
-  // const squares = document.getElementsByTagName("td");
-  // // console.log(squares);
-
-  // // 2. add clicks to each element
-  // for (let i = 0; i < squares.length; i++) {
-
-  //   squares[i].addEventListener("click", function (e) {
-  //     // 3. when you click a square, fill with an X
-  //     // 4. when you click the next square, fill with O      
-  //     console.log(e.target)
-  //   })
-  // }
-
 })
