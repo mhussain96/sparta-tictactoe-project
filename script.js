606f92fed3aa31cb48262e4af79eeca5276ab5f2
Maiwand <mhussain2@spartaglobal.com>
2019-01-17 00:05:10 +0000

    added winning combinations

--- script.js
+++ script.js
@@ -4,29 +4,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const player1 = 'X';
   const player2 = 'O';
   const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
+    [0, 1, 2],   // 0
+    [3, 4, 5],   // 1
+    [6, 7, 8],   // 2
+    [0, 3, 6],   // 3
+    [1, 4, 7],   // 4
+    [2, 5, 8],   // 5
+    [0, 4, 8],   // 6
+    [6, 4, 2]    // 7
+  ]  // array of arrays
 
   const squares = document.querySelectorAll('td');
-
+  
   for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function(e) {
       // document.getElementsByTagName()
       // console.log(e.target);
-      e.target.className = "X";
-      e.target.innerText = 'X';
 
       if (playerTurn) {
+        e.target.className = 'X';
+        e.target.innerText = 'X';
+        playerTurn = false; // when player1 fills box this statement allows player2 to fill box
+      } else if (!playerTurn) {
         e.target.className = 'O';
         e.target.innerText = 'O';
+        playerTurn = true;
       }
+      for (let i = 0; i < winCombos.length; i++) { // for loop for winning combos
+        if (squares[winCombos[i][0]].innerText == 'X' &&
+        squares[winCombos[i][1]].innerText == 'X' &&
+        squares[winCombos[i][2]].innerText == 'X') {
+        alert('X Wins!'); 
+        } else if (squares[winCombos[i][0]].innerText == 'O' &&
+        squares[winCombos[i][1]].innerText == 'O' &&
+        squares[winCombos[i][2]].innerText == 'O') {
+        alert('O Wins!')  
+        }
+      }
+
     })
   }
   
